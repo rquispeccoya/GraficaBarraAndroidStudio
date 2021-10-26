a04766dabe29e55026d9abecbb1876fbe5a8efda
Royer <rquispeccoya@gmail.com>
2021-10-26 17:46:53 -0500

    Integracion de los archivos XML de SVG de las iniciales de los nombres de cada integrante

--- app/src/main/java/com/lab05/graficobarras/MainActivity.java
+++ app/src/main/java/com/lab05/graficobarras/MainActivity.java
@@ -11,10 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int [] cantidad ={5,60,80};
-
-        String[]tags={"perro","gato","perico"};
-        int color= ContextCompat.getColor(this, R.color.teal_200);
+        int [] cantidad ={5,60,80,10,4,3,8,20,86,91};
+        //int [] cantidad ={70};
+        String[]tags={"perro","gato","perico","elefante","liebre","pericote","raton","xx","yy","nn"};
+        //String []tags={"perro"};
+        int color= ContextCompat.getColor(this, R.color.rojo);
         setContentView(new GraficoBarras(this,cantidad,tags,color));
 
         //GraficoBarras graficoBarras = new GraficoBarras(this,cantidad,tags);
